664758a0d2e1e576c15ca1cfc144bc3cd888f1db
Kirill Makeev <makeevbang@gmail.com>
2025-02-04 15:50:17 +0300

    Django lesson 4+5

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -5,5 +5,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# def index(request):
+#     return HttpResponse("asd")
+
+
 def index(request):
-    return HttpResponse('article')
+    app_name = "articlesssssssssssss"
+    return render(
+        request,
+        'articles/index.html',
+        context={"app_name": app_name}
+    )
